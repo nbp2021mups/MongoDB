2963c5c9dfa690fea7e3541c6090431c9736f012
sssteeefaaan <stefan.sa.aleksic@gmail.com>
2022-02-02 00:58:35 +0100

    Postman requests

--- EKnjizara/backend/routes/products.js
+++ EKnjizara/backend/routes/products.js
@@ -3,54 +3,79 @@ const router = express.Router();
 
 const { ProductModel } = require("../models/productsModel");
 
-router.get("/search", async(req, res) => {
+router.get("/search", (req, res) => {
     try {
         ProductModel.find(JSON.parse(req.query.filter))
             .select(req.query.select)
             .skip(req.query.skip)
             .limit(req.query.count)
             .then((result) => {
-                console.log(result);
-                res.send({ msg: "Success!", content: result });
+                return res.send({ msg: "Success!", content: result });
+            })
+            .catch((err) => {
+                console.log(err);
+                return res
+                    .status(409)
+                    .send({ msg: "Error occurred!", content: err.message });
             });
     } catch (ex) {
         console.log(ex);
-        return res.status(409).send({ msg: "Error occurred!", content: ex });
+        return res.status(501).send({ msg: "Server error occurred!", content: ex });
     }
 });
 
-router.post("/", async(req, res) => {
+router.post("/", (req, res) => {
     try {
-        new ProductModel(req.body).save().then((response) => {
-            return res.send({ msg: "Success!", content: response });
-        });
-    } catch (ex) {
-        console.log(ex);
-        return res.status(409).send({ msg: "Error occurred!", content: ex });
-    }
-});
-
-router.put("/", async(req, res) => {
-    try {
-        ProductModel.updateOne(req.body.filter, req.body.update).then(
-            (response) => {
+        new ProductModel(req.body)
+            .save()
+            .then((response) => {
                 return res.send({ msg: "Success!", content: response });
-            }
-        );
+            })
+            .catch((err) => {
+                console.log(err);
+                return res
+                    .status(409)
+                    .send({ msg: "Error occurred!", content: err.message });
+            });
     } catch (ex) {
         console.log(ex);
-        return res.status(409).send({ msg: "Error occurred!", content: ex });
+        return res.status(501).send({ msg: "Server error occurred!", content: ex });
     }
 });
 
-router.delete("/", async(req, res) => {
+router.put("/", (req, res) => {
     try {
-        ProductModel.deleteOne(JSON.parse(req.query.filter)).then((response) => {
-            return res.send({ msg: "Success!", content: response });
-        });
+        ProductModel.updateOne(req.body.filter, req.body.update)
+            .then((response) => {
+                return res.send({ msg: "Success!", content: response });
+            })
+            .catch((err) => {
+                console.log(err);
+                return res
+                    .status(409)
+                    .send({ msg: "Error occurred!", content: err.message });
+            });
     } catch (ex) {
         console.log(ex);
-        return res.status(409).send({ msg: "Error occurred!", content: ex });
+        return res.status(501).send({ msg: "Server error occurred!", content: ex });
+    }
+});
+
+router.delete("/", (req, res) => {
+    try {
+        ProductModel.deleteOne(JSON.parse(req.query.filter))
+            .then((response) => {
+                return res.send({ msg: "Success!", content: response });
+            })
+            .catch((err) => {
+                console.log(err);
+                return res
+                    .status(409)
+                    .send({ msg: "Error occurred!", content: err.message });
+            });
+    } catch (ex) {
+        console.log(ex);
+        return res.status(501).send({ msg: "Server error occurred!", content: ex });
     }
 });
 
