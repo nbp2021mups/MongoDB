0d441d03a1bfdcfe716c4f9e778aae1be89bd0c1
unknown <pesic.uros@hotmail.com>
2022-02-05 22:55:35 +0100

    a

--- EBookstore/src/app/app.module.ts
+++ EBookstore/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegistrationPageComponent } from './registration-page/registration-page.component';
 import { HeaderComponent } from './header/header.component';
-import { MatCardModule } from '@angular/material/card';
 import { ProductComponent } from './products/product/product.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
 
@@ -23,8 +22,7 @@ import { ProductListComponent } from './products/product-list/product-list.compo
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule,
-    MatCardModule
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
