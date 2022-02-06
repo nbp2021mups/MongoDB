0e7666965980904a1d5d1775db5dc3dbec0fcaf0
miljanasimic <simic.mimi@gmail.com>
2022-02-06 05:22:34 +0100

    rute

--- EBookstore/src/app/app-routing.module.ts
+++ EBookstore/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductPageComponent } from './products/product-page/product-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'knjige', component: ProductPageComponent},
-  { path: 'proizvodi', component: ProductPageComponent}
+  { path: 'proizvodi', component: ProductPageComponent},
+  { path: 'prijavljivanje', component: LoginPageComponent},
+  { path: 'registracija', component: RegistrationPageComponent}
 ];
 
 @NgModule({
