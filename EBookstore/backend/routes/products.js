5cad89924ee77d4b08dc01bb2f84840ad03e47f1
sssteeefaaan <stefan.sa.aleksic@gmail.com>
2022-02-07 22:32:12 +0100

    Product add

--- EBookstore/backend/routes/products.js
+++ EBookstore/backend/routes/products.js
@@ -6,7 +6,8 @@ const fs = require("fs");
 
 const { ProductModel } = require("../models/productsModel");
 const { CompanyModel } = require("../models/companiesModel");
-const { ObjectId } = require("mongoose").Types;
+const { UserModel } = require("../models/usersModel");
+const mongoose = require('mongoose');
 
 router.get("/search", (req, res) => {
     try {
@@ -46,39 +47,45 @@ router.post("/", multer({ storage }).single("file"), async(req, res) => {
             });
         }
 
-        const _id = new ObjectId();
-        if (req.body.poreklo.naziv) {
-            const update = await CompanyModel.updateOne({ _id: req.body.poreklo.id }, { $push: { ponudjeniProizvodi: _id } });
-            if (update.modifiedCount == 0) {
-                if (req.file) fs.unlinkSync(req.file.path);
-                return res.status(409).send({
-                    poruka: "Nastala je greska!",
-                    sadrzaj: "Ne postoji knjizara sa prosledjenim identifikatorom!",
-                });
-            }
+        const session = await mongoose.startSession();
+        session.startTransaction();
+
+        try {
+            const product = await new ProductModel({
+                    ...req.body,
+                    slika: req.protocol +
+                        "://" +
+                        req.get("host") +
+                        "/images/" +
+                        (req.file ?
+                            req.file.filename :
+                            "template-" + req.body.kategorija + ".png"),
+                })
+                .save();
+
+            let update;
+            if (req.body.poreklo.naziv)
+                update = await CompanyModel.updateOne({ _id: req.body.poreklo.id }, { $push: { ponudjeniProizvodi: product._id } });
+            else if (req.body.kategorija == "knjiga za iznajmljivanje")
+                update = await UserModel.updateOne({ _id: req.body.poreklo.id }, { $push: { iznajmljuje: product._id } });
+            else
+                throw "Korisnici mogu samo knjige da postavljaju na iznajmljivanje!";
+
+            if (update.modifiedCount == 0)
+                throw "Neispravno poreklo!";
+
+            await session.commitTransaction();
+            await session.endSession();
+
+            return res.send({ poruka: "Uspesno", sadrzaj: product._doc });
+        } catch (sadrzaj) {
+            if (req.file)
+                fs.unlinkSync(req.file.path);
+
+            await session.abortTransaction();
+            await session.endSession();
+            return res.status(409).send({ poruka: "Nastala je greska!", sadrzaj });
         }
-        new ProductModel({
-                ...req.body,
-                _id,
-                slika: req.protocol +
-                    "://" +
-                    req.get("host") +
-                    "/images/" +
-                    (req.file ?
-                        req.file.filename :
-                        "template-" + req.body.kategorija + ".png"),
-            })
-            .save()
-            .then((response) => {
-                return res.send({ poruka: "Uspesno!", sadrzaj: response });
-            })
-            .catch((err) => {
-                if (req.file) fs.unlinkSync(req.file.path);
-                console.log(err);
-                return res
-                    .status(409)
-                    .send({ poruka: "Nastala je greska!", sadrzaj: err.message });
-            });
     } catch (ex) {
         if (req.file) fs.unlinkSync(req.file.path);
         console.log(ex);
