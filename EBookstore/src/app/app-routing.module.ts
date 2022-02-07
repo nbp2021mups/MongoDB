12526f37e12abde0ab19805062c57a81b49cf512
miljanasimic <simic.mimi@gmail.com>
2022-02-07 03:59:58 +0100

    home page

--- EBookstore/src/app/app-routing.module.ts
+++ EBookstore/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductPageComponent } from './products/product-page/product-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegistrationPageComponent } from './registration-page/registration-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/pocetna', pathMatch: 'full' },
+  {path : 'pocetna', component : HomePageComponent},
   { path: 'knjige', component: ProductPageComponent},
   { path: 'proizvodi', component: ProductPageComponent},
   { path: 'prijavljivanje', component: LoginPageComponent},
--- EBookstore/src/app/home-page/home-page.component.ts
+++ EBookstore/src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  register(){
+    this.router.navigate(['registracija']);
+  }
+
+  bookstores(){
+    this.router.navigate(['knjizare']);
+  }
+
+  books(){
+    this.router.navigate(['knjige']);
+  }
+
+  rent(){
+    this.router.navigate(['iznajmljivanje']);
+  }
+
+  products(){
+    this.router.navigate(['proizvodi']);
+  }
 }
