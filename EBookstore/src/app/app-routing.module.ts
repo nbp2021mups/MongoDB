3421988ea81dc69e3cd5e6aebd8b1a0c9ae1c6c9
unknown <pesic.uros@hotmail.com>
2022-02-10 16:57:14 +0100

    auth guards

--- EBookstore/src/app/app-routing.module.ts
+++ EBookstore/src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { RegistrationPageComponent } from './registration-page/registration-page
 import { HomePageComponent } from './home-page/home-page.component';
 import { BookstoreListComponent } from './components/bookstore-list/bookstore-list.component';
 import { CartComponent } from './components/cart/cart.component';
-import { AuthGuard } from 'src/services/auth-guard.service';
+import { AuthGuardUser } from 'src/services/auth-guard-user.service';
+import { AuthGuardBookstore } from 'src/services/auth-guard-bookstore.service';
 import { AddProductComponent } from './products/add-product/add-product.component';
 import { EditProductComponent } from './products/edit-product/edit-product.component';
 import { ProductInfoPageComponent } from './product-info-page/product-info-page.component';
 import { OrdersComponent } from './components/orders/orders.component';
 import { AddUsersBookComponent } from './add-users-book/add-users-book.component';
+import { AuthGuard } from 'src/services/auth-guard.service';
 
 const routes: Routes = [
   { path: '', redirectTo: '/pocetna', pathMatch: 'full' },
@@ -22,13 +24,13 @@ const routes: Routes = [
   { path: 'proizvodi/:idKnjizare', component: ProductPageComponent },
   { path: 'prijavljivanje', component: LoginPageComponent },
   { path: 'registracija', component: RegistrationPageComponent },
-  { path: 'knjizare', component: BookstoreListComponent },
-  { path: 'novi-proizvod', component: AddProductComponent },
-  { path: 'korpa', component: CartComponent, canActivate: [AuthGuard] },
-  { path: 'narudzbine', component: OrdersComponent },
-  { path: 'izmena/:idProizvoda', component: EditProductComponent},
+  { path: 'knjizare', component: BookstoreListComponent, canActivate: [AuthGuardUser] },
+  { path: 'novi-proizvod', component: AddProductComponent, canActivate: [AuthGuardBookstore] },
+  { path: 'korpa', component: CartComponent, canActivate: [AuthGuardUser] },
+  { path: 'narudzbine', component: OrdersComponent, canActivate: [AuthGuard] },
+  { path: 'izmena/:idProizvoda', component: EditProductComponent, canActivate: [AuthGuardBookstore]},
   { path: 'proizvod/:idProizvoda', component: ProductInfoPageComponent},
-  { path: 'nova-knjiga-iznajmljivanje', component: AddUsersBookComponent},
+  { path: 'nova-knjiga-iznajmljivanje', component: AddUsersBookComponent, canActivate: [AuthGuardUser]},
   { path: '**', redirectTo: 'pocetna' },
 ]
 
